5d2382258ca9fcaec3439b3a109b8b030b8aaa31
jwsscott777 <tlmsols50@gmail.com>
2025-01-04 17:22:17 -0500

    Add Cors stuff

--- pages/api/tasks/index.js
+++ pages/api/tasks/index.js
@@ -4,6 +4,14 @@ import {revalidatePath} from "next/cache";
 export const revalidate = 0;
 export default async function handler(req, res) {
 
+  res.setHeader("Access-Control-Allow-Origin", "https://cert-management-ten.vercel.app"); // Replace with your actual frontend URL
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS"); // Allowed methods
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allowed headers
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
   const {userId} = getAuth(req);
   if(!userId){
     revalidatePath("/");
